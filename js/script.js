e937f421b2d919842073c3dd4d92f5a23345491d
Emiliya Dobrogorska <emiliyadqa@gmail.com>
2024-06-12 14:55:57 -0700

    Refactor js file

--- js/script.js
+++ js/script.js
@@ -1,50 +1,78 @@
-const input_todo = document.querySelector("input");
+const btnAddTask = document.querySelector("#btn-add");
+const inputTodo = document.querySelector(".input--todo");
+const listTask = document.querySelector(".list");
 
-document.querySelector("#btn-add").addEventListener("click", () => {
-  const inputData = input_todo.value;
-  addTask(inputData);
-  input_todo.value = "";
-});
-
-const addTask = (task) => {
-  if (task.length > 0) {
-    const list = document.querySelector(".list");
-
-    const listItem = document.createElement("li");
-    listItem.className = "list__item";
-    list.appendChild(listItem);
-
-    const listContent = document.createTextNode(task);
-    listItem.appendChild(listContent);
-
-    const iconContainer = document.createElement("div");
-    iconContainer.className = "icon-container";
-
-    const btnDone = document.createElement("button");
-    btnDone.className = "btn-icon";
-    const doneIcon = document.createElement("span");
-    doneIcon.className = "material-symbols-outlined icon icon--done";
-    doneIcon.textContent = "check_circle";
-    btnDone.appendChild(doneIcon);
-
-    const btnEdit = document.createElement("button");
-    btnEdit.className = "btn-icon";
-    const editIcon = document.createElement("span");
-    editIcon.className = "material-symbols-outlined icon icon--edit";
-    editIcon.textContent = "edit";
-    btnEdit.appendChild(editIcon);
-
-    const btnDelete = document.createElement("button");
-    btnDelete.className = "btn-icon";
-    const deleteIcon = document.createElement("span");
-    deleteIcon.className = "material-symbols-outlined icon icon--delete";
-    deleteIcon.textContent = "delete";
-    btnDelete.appendChild(deleteIcon);
-
-    iconContainer.appendChild(btnDone);
-    iconContainer.appendChild(btnEdit);
-    iconContainer.appendChild(btnDelete);
-
-    listItem.appendChild(iconContainer);
+const handleAddTask = () => {
+  const task = inputTodo.value.trim();
+  if (task) {
+    addTask(task);
+    inputTodo.value = "";
   }
 };
+btnAddTask.addEventListener("click", handleAddTask);
+
+const addTask = (task) => {
+  const listItem = createListItem(task);
+  listTask.appendChild(listItem);
+};
+
+//Create List_item element with class list_item and textContext
+const createListItem = (content) => {
+  const listItem = document.createElement("li");
+  listItem.className = "list__item";
+  if (content) {
+    listItem.textContent = content;
+  }
+
+  const iconContainer = createIconContainer();
+  listItem.appendChild(iconContainer);
+
+  return listItem;
+};
+
+//Create div class="icon-container"
+const createIconContainer = () => {
+  const iconContainer = document.createElement("div");
+  iconContainer.className = "icon-container";
+
+  const btnDone = createBtnIcon("check_circle", "icon--done", handleDoneTask);
+  const btnEdit = createBtnIcon("edit", "icon--edit");
+  const btnDelete = createBtnIcon("delete", "icon--delete", handleDeleteTask);
+
+  iconContainer.appendChild(btnDone);
+  iconContainer.appendChild(btnEdit);
+  iconContainer.appendChild(btnDelete);
+
+  return iconContainer;
+};
+
+//Create button class="btn-icon" with span class="material-symbols-outlined icon (icon--done/icon--edit/icon--delete) and textContext (check_circle/ edit/delete)
+
+const createBtnIcon = (iconText, iconClass, eventHandler) => {
+  const btnIcon = document.createElement("button");
+  btnIcon.className = "btn-icon";
+
+  const icon = document.createElement("span");
+  icon.className = `material-symbols-outlined icon ${iconClass}`;
+  icon.textContent = iconText;
+
+  btnIcon.appendChild(icon);
+  btnIcon.addEventListener("click", eventHandler);
+
+  return btnIcon;
+};
+
+const handleDoneTask = (event) => {
+  const listItem = event.target.closest(".list__item");
+  listTask.removeChild(listItem);
+};
+
+const handleDeleteTask = (event) => {
+  const listItem = event.target.closest(".list__item");
+  listTask.removeChild(listItem);
+};
+
+// console.log(addTask("to do"));
+// console.log(createListItem("To do"));
+// console.log(createIconContainer());
+// console.log(createBtnIcon("edit", "icon--edit"));
